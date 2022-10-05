ec2654754b6a22b45a70aca93147b422b03c24e0
KolesnikovaDi <kolesnikovadiana0559@gmail.com>
2022-10-05 19:50:07 +0300

    Add Stats Card themes

---
+++ index.js
@@ -0,0 +1,12 @@
+radical: {
+    title_color: "fe428e";
+    icon_color: "f8d847";
+    text_color: "a9fef7";
+    bg_color: "141321";
+  };
+  cobalt: {
+    title_color:"e683d9";
+    icon_color:"0480ef";
+    text_color:"75eeb2";
+    bg_color:"193549";
+  }
